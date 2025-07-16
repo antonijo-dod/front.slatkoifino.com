1a7e14030dfec2bb6785cdd4fa45c4539878029b
Antonijo Dod <a.djodj@gmail.com>
2025-07-16 19:39:05 +0100

    and again fix

--- next.config.ts
+++ next.config.ts
@@ -12,12 +12,19 @@ const nextConfig: NextConfig = {
     ],
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "**",
-        port: "",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/slatko-i-fino/image/upload/**', // Your specific Cloudinary path
+      },
+      {
+        protocol: 'http', // Or 'https' if your Strapi instance uses it
+        hostname: 'r8og4gk0g040kcgow48ooocg.91.99.119.7.sslip.io', // The domain from your API_URL
+        port: '1337', // If your URL has a port
+        pathname: '/uploads/**',
       },
     ],
+  },
   },
   typescript: {
     // Ignore TypeScript errors during build
