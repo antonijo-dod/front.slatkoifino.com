a1fc2ae7587ded60ebc632a6b4020749edc657e9
antonijo-dod <127656303+antonijo-dod@users.noreply.github.com>
2025-08-03 12:56:49 +0100

    feat: implement responsive navbar

--- src/app/(subpages)/layout.tsx
+++ src/app/(subpages)/layout.tsx
@@ -1,8 +1,7 @@
 import type React from "react";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
 import Image from "next/image";
+import Navbar from "@/components/navbar";
 
 export default function RootLayout({
   children,
@@ -11,54 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <nav className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50">
-        <div className="max-w-7xl mx-auto px-4">
-          <div className="flex items-center justify-between h-16">
-            <Link
-              href="/"
-              className="flex items-center gap-2 font-bold relative text-xl"
-            >
-              <Image
-                src="/images/slatkoIfino-logo.png"
-                alt="Sweet Creations Logo"
-                width={100}
-                height={30}
-              />
-            </Link>
-
-            <div className="hidden md:flex items-center gap-6">
-              <Link
-                href="/"
-                className="text-sm font-medium hover:text-pink-600 transition-colors"
-              >
-                Pocetna
-              </Link>
-              <Link
-                href="/recepti"
-                className="text-sm font-medium hover:text-pink-600 transition-colors"
-              >
-                Svi recepti
-              </Link>
-              {/* <Link
-                href="/about"
-                className="text-sm font-medium hover:text-pink-600 transition-colors"
-              >
-                About
-              </Link> */}
-              {/* <Link
-                href="/kontakt"
-                className="text-sm font-medium hover:text-pink-600 transition-colors"
-              >
-                Kontakt
-              </Link> */}
-            </div>
-
-            <Button variant="ghost" size="icon" className="md:hidden">
-              <Menu className="w-5 h-5" />
-            </Button>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
 
       {children}
 
