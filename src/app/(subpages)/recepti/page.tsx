30a44f20371077fa901507f92f6e95e4f2977bbc
antonijo-dod <a.djodj@gmail.com>
2026-06-25 10:45:12 +0100

    fix: optimize image loading priority for recipe cards

--- src/app/(subpages)/recepti/page.tsx
+++ src/app/(subpages)/recepti/page.tsx
@@ -38,8 +38,8 @@ export default async function RecipesPage({ searchParams }: { searchParams: Prom
         <div className="mt-12">
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
             {/* @ts-expect-error Replace any with appropriate type */}
-            {recipeResponse.data.map((recipe) => (
-              <RecipeCard key={recipe.id} recipe={recipe} />
+            {recipeResponse.data.map((recipe, i) => (
+              <RecipeCard key={recipe.id} recipe={recipe} priority={i < 3} />
             ))}
           </div>
           <Pagination currentPage={currentPage} pageCount={pageCount} />
