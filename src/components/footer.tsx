bf001c5d1baaa5c027c41d568f64bb45ed9ddda4
antonijo-dod <a.djodj@gmail.com>
2026-07-15 14:38:29 +0100

    fix: remove unused Menu import from footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Menu, Facebook } from "lucide-react";
+import { Facebook } from "lucide-react";
 
 function Footer() {
   return (
