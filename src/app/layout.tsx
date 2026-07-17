cc3fbfe9993e58ba0fb496734b85acf876e6e953
antonijo-dod <a.djodj@gmail.com>
2026-07-17 14:20:51 +0100

    fix: wrap Cloudflare analytics scripts in production check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,17 +31,21 @@ export default function RootLayout({
       >
         {children}
       </body>
-      <Script
-        strategy="afterInteractive"
-        data-domain="slatkoifino.com"
-        src="https://metrics.slatkoifino.com/js/script.js"
-      />
-      <Script
-        strategy="afterInteractive"
-        type="module"
-        src="https://static.cloudflareinsights.com/beacon.min.js"
-        data-cf-beacon='{"token": "0beb3cc56b984ddcb2a4b2e8ec46a1e5"}'
-      />
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            data-domain="slatkoifino.com"
+            src="https://metrics.slatkoifino.com/js/script.js"
+          />
+          <Script
+            strategy="afterInteractive"
+            type="module"
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "0beb3cc56b984ddcb2a4b2e8ec46a1e5"}'
+          />
+        </>
+      )}
     </html>
   );
 }
